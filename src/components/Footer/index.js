3f051d847acad9d1aaf216db9375f7a36e27080d
Pablo Diaz Garcia <pablodiazga@hotmail.com>
2021-05-04 13:35:35 +0200

    react router / custom layouts / footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="wrap">Pablo Diaz</div>
+    </footer>
+  );
+};
+
+export default Footer;
