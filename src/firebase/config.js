b05de4d31c7c6e9e6495afad2813cfb78aa8121e
Pablo Diaz Garcia <pablodiazga@hotmail.com>
2021-05-05 10:47:27 +0200

    Google sign-in

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKT9GS0IwSWf1jAa8kqZtsIGMb8MicWsI",
+  authDomain: "ecommerce-website-a3745.firebaseapp.com",
+  projectId: "ecommerce-website-a3745",
+  storageBucket: "ecommerce-website-a3745.appspot.com",
+  messagingSenderId: "377620955270",
+  appId: "1:377620955270:web:3f673691bd37ca88225107",
+};
